fc8ded6df04afbb13b99023d687b825c894df499
sk7pxcb2tj-web <sk7pxcb2tj@privaterelay.appleid.com>
2026-05-27 19:18:51 +0700

    add src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { redirect } from 'next/navigation'
+import { cookies } from 'next/headers'
+import { createClient } from '@/utils/supabase/server'
+
+export default async function Home() {
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
+  const { data: { user } } = await supabase.auth.getUser()
+  if (user) redirect('/score')
+  else redirect('/login')
+}
